addfdf4705f9ebc7c7d64109ced9d59cc224ed44
Morten Sickel <morten@sickel.net>
2018-05-23 10:18:19 +0200

    Preparations to make multi-roi runs

--- processdata.py
+++ processdata.py
@@ -1,6 +1,22 @@
 import sys
 import csv
 import ntpath
+import collections
+
+
+# TODO: Add several sets of ROIs
+# Eg: processdata <file> 20,30,40 80,90,100,110
+# Makes a two window set with ch 20-30 and ch 30-40 and a three window set with ch 80-90, ch 90-100 and ch 100-110
+# These should be named ROI_01_1, ROI_01_2, ROI_02_1 etc.
+# Possible to name ROIS on command line: 
+# Eg: processdata <file> 20,30,40,R1 80,90,100,110,R2
+# These should be named R1_1, R1_2, R2_1 etc.
+
+def tree():
+    return collections.defaultdict(tree)
+    # Makes handling of multidimentional dicts easier
+
+rois=tree()
 
 if len(sys.argv)==1:
     print("Oppgi fil som skal prosseseres - første og siste kanal")
@@ -15,6 +31,12 @@ if len(sys.argv)>3:
     lastch=int(sys.argv[3])
 else:
     lastch=1023
+
+roiname="_".join(["ROI",str(firstch),str(lastch)])
+rois[roiname]=[firstch,lastch]
+
+    
+
 print(firstch,lastch)
 basename=ntpath.basename(readfile)
 writefile="proc_"+str(firstch)+"_"+str(lastch)+"_"+basename
@@ -33,8 +55,11 @@ with open(readfile) as rf:
             latix=line.index("Lat")
             altix=line.index("Alt[m]")
             wf=open(writefile,"w")
-            wf.write("lat,lon,alt,Ch_"+str(firstch)+"_"+str(lastch)+"\n")
+            header="lat,lon,alt"
+            roilist=",".join(rois.keys())
+            wf.write(header+","+roilist+"\n")
         if lnr>3:
+        #TODO: Use rois[] to get firstch and lastch
             data=list(map(int,line[ch00+firstch:ch00+lastch+1]))
             wf.write(",".join([line[latix],line[lonix],line[altix],str(sum(data))]))
             wf.write("\n")
