6c607c1feed1187c3c142167140f3c4b5f65095a
Morten Sickel <morten@sickel.net>
2018-05-23 09:48:03 +0200

    Better handling of command line arguments

--- processdata.py
+++ processdata.py
@@ -2,17 +2,24 @@ import sys
 import csv
 import ntpath
 
-if len(sys.argv)<4:
+if len(sys.argv)==1:
     print("Oppgi fil som skal prosseseres - første og siste kanal")
     sys.exit(1)
     
 readfile=sys.argv[1]
-firstch=int(sys.argv[2])
-lastch=int(sys.argv[3])
+if len(sys.argv)>2:
+    firstch=int(sys.argv[2])
+else:
+    firstch=0
+if len(sys.argv)>3:
+    lastch=int(sys.argv[3])
+else:
+    lastch=1023
 print(firstch,lastch)
 basename=ntpath.basename(readfile)
 writefile="proc_"+str(firstch)+"_"+str(lastch)+"_"+basename
-print(basename)
+print("Reading: "+basename)
+print("Writing: "+writefile)
 lnr=0
 ch00=-1
 wf=None
@@ -25,7 +32,6 @@ with open(readfile) as rf:
             lonix=line.index("Long")
             latix=line.index("Lat")
             altix=line.index("Alt[m]")
-            print(ch00,latix,lonix,altix)
             wf=open(writefile,"w")
             wf.write("lat,lon,alt,Ch_"+str(firstch)+"_"+str(lastch)+"\n")
         if lnr>3:
